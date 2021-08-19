ac3b936fa86f201da5978347b9899fe68152bbde
Arnel Lenteria <arnel.lenteria@gmail.com>
2021-08-19 22:15:39 +0800

    modified:   index.js

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-var config = require('./config.js')
 var router = require('./router.js')
 var { app } = require('plugin-core')
 
 module.exports = {
   async init () {
-    config.init()
     app.use(router)
   },
   uninstall () {
     // called with you uninstall the plugin
   }
 }
-
